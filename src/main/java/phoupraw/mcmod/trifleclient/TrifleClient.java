5cb290543e904e920d382ea3bf5ca1b80f8ff058
Phoupraw <467934387@qq.com>
2024-11-09 19:13:56 +0800

    前移方块高亮渲染时机。

--- src/main/java/phoupraw/mcmod/trifleclient/TrifleClient.java
+++ src/main/java/phoupraw/mcmod/trifleclient/TrifleClient.java
@@ -7,6 +7,7 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
@@ -24,7 +25,6 @@ import phoupraw.mcmod.trifleclient.events.OnUseKeyPress;
 import phoupraw.mcmod.trifleclient.misc.*;
 
 import java.lang.invoke.MethodHandles;
-import java.util.Arrays;
 //TODO 使在流体中行走不减速
 //TODO 稀有度不为常见或有自定义名称的物品始终发光，其它物品和经验球则有不可穿墙的发光轮廓
 
@@ -35,16 +35,20 @@ public final class TrifleClient implements ModInitializer, ClientModInitializer 
     public static final String ID = "trifleclient";
     public static final String NAME_KEY = "modmenu.nameTranslation." + ID;
     @ApiStatus.Internal
-    public static final Logger LOGGER = LogManager.getLogger(ID);
+    public static final Logger LOGGER = LogManager.getLogger();
     @SneakyThrows
-    private static void loadClasses() {
-        for (var cls : Arrays.asList(TargetPointer.class, BlockFinder.class, BlockHighlighter.class, TCKeyBindings.class, MiningDelay.class, AutoAttacker.class, MiningSame.class)) {
-            MethodHandles.lookup().ensureInitialized(cls);
-        }
+    static void loadClass(Class<?> cls) {
+        MethodHandles.lookup().ensureInitialized(cls);
     }
     @Override
     public void onInitializeClient() {
-        loadClasses();
+        loadClass(TargetPointer.class);
+        loadClass(BlockFinder.class);
+        WorldRenderEvents.BEFORE_DEBUG_RENDER.register(BlockHighlighter::afterEntities);
+        loadClass(TCKeyBindings.class);
+        loadClass(MiningDelay.class);
+        loadClass(AutoAttacker.class);
+        loadClass(MiningSame.class);
         ServerLifecycleEvents.SERVER_STARTING.register(server -> server.setFlightEnabled(false));
         AfterClientPlayerMove.EVENT.register(NormalSpeed::afterClientPlayerMove);
         OnClientPlayerMove.EVENT.register(SpeedSpeed::onClientPlayerMove);
@@ -58,6 +62,7 @@ public final class TrifleClient implements ModInitializer, ClientModInitializer 
         ClientTickEvents.START_WORLD_TICK.register(SprucePlanter::onStartAndEndTick);
         ClientTickEvents.END_WORLD_TICK.register(SprucePlanter::onStartAndEndTick);
         if (FabricLoader.getInstance().isModLoaded(MekanismCompact.MOD_ID)) {
+            TrifleClient.LOGGER.info("检测到《通用机械》，将加载相关兼容。");
             AutoAttacker.WEAPON.register(MekanismCompact::isWeapon);
         }
     }
--- src/main/java/phoupraw/mcmod/trifleclient/misc/BlockHighlighter.java
+++ src/main/java/phoupraw/mcmod/trifleclient/misc/BlockHighlighter.java
@@ -1,25 +1,24 @@
 package phoupraw.mcmod.trifleclient.misc;
 
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
-import lombok.experimental.UtilityClass;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
+import org.jetbrains.annotations.ApiStatus;
 
 import java.util.OptionalDouble;
 import java.util.Set;
 
-@UtilityClass
-public class BlockHighlighter {
-    public static final Set<BlockBox> BLOCK_BOXES = new ObjectOpenHashSet<>();
+@ApiStatus.NonExtendable
+public interface BlockHighlighter {
+    Set<BlockBox> BLOCK_BOXES = new ObjectOpenHashSet<>();
     /**
      <a href=https://github.com/Earthcomputer/clientcommands/blob/fabric/src/main/java/net/earthcomputer/clientcommands/render/RenderQueue.java>Client Commands</a>
      */
-    public static RenderLayer NO_DEPTH_LAYER = RenderLayer.of("clientcommands_no_depth", VertexFormats.LINES, VertexFormat.DrawMode.LINES, 256, true, true, RenderLayer.MultiPhaseParameters.builder()
+    RenderLayer NO_DEPTH_LAYER = RenderLayer.of("clientcommands_no_depth", VertexFormats.LINES, VertexFormat.DrawMode.LINES, 256, true, true, RenderLayer.MultiPhaseParameters.builder()
       .program(RenderPhase.LINES_PROGRAM)
       .writeMaskState(RenderPhase.COLOR_MASK)
       .cull(RenderPhase.DISABLE_CULLING)
@@ -27,18 +26,18 @@ public class BlockHighlighter {
       .layering(RenderPhase.VIEW_OFFSET_Z_LAYERING)
       .lineWidth(new RenderPhase.LineWidth(OptionalDouble.of(2)))
       .build(true));
-    static {
-        WorldRenderEvents.AFTER_TRANSLUCENT.register(BlockHighlighter::afterEntities);
-    }
-    private static void afterEntities(WorldRenderContext context) {
+    @ApiStatus.Internal
+    static void afterEntities(WorldRenderContext context) {
         MatrixStack matrices = context.matrixStack();
-        VertexConsumer buffer = context.consumers().getBuffer(NO_DEPTH_LAYER);
+        if (matrices == null) return;
+        VertexConsumerProvider consumers = context.consumers();
+        if (consumers == null) return;
+        VertexConsumer buffer = consumers.getBuffer(NO_DEPTH_LAYER);
+        if (buffer == null) return;
         Vec3d offset = context.camera().getPos().multiply(-1);
-        //RenderSystem.blendFuncSeparate(GlStateManager.SrcFactor.ONE_MINUS_DST_COLOR, GlStateManager.DstFactor.ONE_MINUS_SRC_COLOR, GlStateManager.SrcFactor.ONE, GlStateManager.DstFactor.ZERO);
         for (BlockBox blockBox : BLOCK_BOXES) {
             Box box = Box.from(blockBox);
             WorldRenderer.drawBox(matrices, buffer, box.offset(offset), 1, 1, 1, 1);
-            //WorldRenderer.drawBox(matrices, buffer, box.contract(1/128.0).offset(offset), 0, 0, 0, 1);
         }
     }
 }
