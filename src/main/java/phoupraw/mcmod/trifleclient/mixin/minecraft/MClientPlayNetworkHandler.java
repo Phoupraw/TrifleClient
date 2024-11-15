4f8f938965d757da503f4beebe2c8ea28a73eac6
Phoupraw <467934387@qq.com>
2024-11-16 05:01:02 +0800

    修复：有时在结束鞘翅飞行时会突然向上看。

--- src/main/java/phoupraw/mcmod/trifleclient/mixin/minecraft/MClientPlayNetworkHandler.java
+++ src/main/java/phoupraw/mcmod/trifleclient/mixin/minecraft/MClientPlayNetworkHandler.java
@@ -1,5 +1,6 @@
 package phoupraw.mcmod.trifleclient.mixin.minecraft;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.fabricmc.api.EnvType;
@@ -11,8 +12,11 @@ import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.entity.player.PlayerAbilities;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.packet.s2c.play.PlayerAbilitiesS2CPacket;
+import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Slice;
+import phoupraw.mcmod.trifleclient.config.TCConfigs;
 import phoupraw.mcmod.trifleclient.mixins.minecraft.MMClientPlayNetworkHandler;
 
 @Environment(EnvType.CLIENT)
@@ -28,5 +32,9 @@ abstract class MClientPlayNetworkHandler extends ClientCommonNetworkHandler {
     @WrapWithCondition(method = "onPlayerAbilities", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerAbilities;allowFlying:Z"))
     private boolean elytraCancelSyncAllowFlying(PlayerAbilities instance, boolean value, @Local(argsOnly = true) PlayerAbilitiesS2CPacket packet) {
         return MMClientPlayNetworkHandler.elytraCancelSyncAllowFlying(instance, value, packet, client);
+    }
+    @ModifyExpressionValue(method = "onPlayerPositionLook", slice = @Slice(from = @At(value = "FIELD", target = "Lnet/minecraft/network/packet/s2c/play/PositionFlag;X_ROT:Lnet/minecraft/network/packet/s2c/play/PositionFlag;")), at = @At(value = "INVOKE", target = "Ljava/util/Set;contains(Ljava/lang/Object;)Z", ordinal = 0))
+    private boolean no90sync(boolean original, @Local(argsOnly = true) PlayerPositionLookS2CPacket packet) {
+        return original && !(TCConfigs.A.isFreeElytraFlying() && packet.getPitch() <= -90);
     }
 }
