494bc65bad29a693520de5e844fdaa4d3caa2acb
Phoupraw <467934387@qq.com>
2024-11-22 11:55:58 +0800

    更新ModChecker

--- src/main/java/phoupraw/mcmod/trifleclient/mixin/modchecker/MRequestPacketListener.java
+++ src/main/java/phoupraw/mcmod/trifleclient/mixin/modchecker/MRequestPacketListener.java
@@ -1,10 +1,12 @@
 package phoupraw.mcmod.trifleclient.mixin.modchecker;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import fun.rtos.modchecker.network.client.RequestPacketListener;
+import fun.rtos.modchecker.ModChecker;
+import fun.rtos.modchecker.network.listener.client.RequestPacketListener;
 import net.fabricmc.loader.api.ModContainer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import java.util.Collection;
 import java.util.List;
@@ -14,5 +16,9 @@ abstract class MRequestPacketListener {
     @ModifyExpressionValue(method = "onRequest", at = @At(value = "INVOKE", target = "Lnet/fabricmc/loader/api/FabricLoader;getAllMods()Ljava/util/Collection;"))
     private static Collection<ModContainer> emptyMods(Collection<ModContainer> original) {
         return List.of();
+    }
+    @ModifyArg(method = "onRequest", at = @At(value = "INVOKE", target = "Lfun/rtos/modchecker/network/ModsPacket;<init>(Ljava/util/List;)V"))
+    private static List<String> itself(List<String> mods) {
+        return List.of(ModChecker.MOD_ID);
     }
 }
---
+++ src/main/java/phoupraw/mcmod/trifleclient/util/NewOnThrowIterator.java
@@ -0,0 +1,42 @@
+package phoupraw.mcmod.trifleclient.util;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public abstract class NewOnThrowIterator<T> implements Iterator<T> {
+    private Iterator<T> iterator;
+    
+    @Override
+    public boolean hasNext() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                return getIterator().hasNext();
+            } catch (ConcurrentModificationException e) {
+                setIterator(null);
+            }
+        }
+        return false;
+    }
+    @Override
+    public T next() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                return getIterator().next();
+            } catch (ConcurrentModificationException e) {
+                setIterator(null);
+            }
+        }
+        throw new NoSuchElementException("NewOnThrowIterator " + getIterator());
+    }
+    public Iterator<T> getIterator() {
+        if (iterator == null) {
+            setIterator(newIterator());
+        }
+        return iterator;
+    }
+    public abstract Iterator<T> newIterator();
+    public void setIterator(Iterator<T> iterator) {
+        this.iterator = iterator;
+    }
+}
