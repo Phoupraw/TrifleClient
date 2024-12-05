494af2aaa0265dfe2e56e916e7c18c0540f319a4
Phoupraw <467934387@qq.com>
2024-12-05 22:00:41 +0800

    0.24.2 尝试修复自动暴击

--- src/main/java/phoupraw/mcmod/trifleclient/misc/AutoCrit.java
+++ src/main/java/phoupraw/mcmod/trifleclient/misc/AutoCrit.java
@@ -8,20 +8,24 @@ import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 import phoupraw.mcmod.trifleclient.config.TCConfigs;
+import phoupraw.mcmod.trifleclient.mixin.minecraft.AEntity;
 
 @ApiStatus.NonExtendable
 public interface AutoCrit {
     @ApiStatus.Internal
     static ActionResult interact(PlayerEntity player0, World world, Hand hand, Entity entity, @Nullable EntityHitResult hitResult) {
-        if (TCConfigs.A().isAutoCrit() && player0 instanceof ClientPlayerEntity player) {
+        if (TCConfigs.A().isAutoCrit() && player0 instanceof ClientPlayerEntity) {
+            var player = (ClientPlayerEntity & AEntity) player0;
             ClientPlayNetworkHandler network = player.networkHandler;
             if (network != null) {
-                network.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(player.getX(), player.getY() + 0.0625, player.getZ(), true));
-                network.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(player.getX(), player.getY(), player.getZ(), false));
+                double y = player.invokeAdjustMovementForCollisions(new Vec3d(0, 1, 0)).getY();
+                network.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(player.getX(), y, player.getZ(), true));
+                network.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(player.getX(), Math.min(y, player.getY() + 0.01), player.getZ(), false));
             }
         }
         return ActionResult.PASS;
