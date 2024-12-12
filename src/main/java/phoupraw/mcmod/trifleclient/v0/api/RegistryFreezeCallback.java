5174e59f1b7efbd4468f6d629f314c3d8f2a2e01
Phoupraw <467934387@qq.com>
2024-12-13 00:26:12 +0800

    0.36.3

--- src/main/java/phoupraw/mcmod/trifleclient/v0/api/RegistryFreezeCallback.java
+++ src/main/java/phoupraw/mcmod/trifleclient/v0/api/RegistryFreezeCallback.java
@@ -2,9 +2,7 @@ package phoupraw.mcmod.trifleclient.v0.api;
 
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
-import org.jetbrains.annotations.ApiStatus;
 
-@ApiStatus.Internal
 public interface RegistryFreezeCallback {
     Event<RegistryFreezeCallback> EVENT = EventFactory.createArrayBacked(RegistryFreezeCallback.class, callbacks -> () -> {
         for (var callback : callbacks) {
