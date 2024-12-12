acd8dadb038311642487f60b68a3aff769f1d718
Phoupraw <467934387@qq.com>
2024-12-12 20:39:31 +0800

    挖掘延迟改用独立计时

--- src/main/java/phoupraw/mcmod/trifleclient/misc/MiningDelay.java
+++ src/main/java/phoupraw/mcmod/trifleclient/misc/MiningDelay.java
@@ -1,38 +1,45 @@
 package phoupraw.mcmod.trifleclient.misc;
 
+import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.UtilityClass;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.block.BlockState;
+import net.fabricmc.fabric.api.event.client.player.ClientPlayerBlockBreakEvents;
+import net.fabricmc.fabric.api.event.client.player.ClientPreAttackCallback;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
 import org.jetbrains.annotations.ApiStatus;
 import phoupraw.mcmod.trifleclient.config.TCConfigs;
 import phoupraw.mcmod.trifleclient.constant.TCKeyBindings;
 import phoupraw.mcmod.trifleclient.mixin.minecraft.AClientPlayerInteractionManager;
-import phoupraw.mcmod.trifleclient.mixin.minecraft.AClientWorld;
+import phoupraw.mcmod.trifleclient.util.MCUtils;
 
 @UtilityClass
 public class MiningDelay {
+    @Getter
     @Setter
     private static boolean on = true;
+    private long lastBreak = -100;
     static {
         ClientTickEvents.START_WORLD_TICK.register(MiningDelay::onStartTick);
+        ClientPreAttackCallback.EVENT.register((client, player, clickCount) -> {
+            if (isOn() && client.crosshairTarget instanceof BlockHitResult hitResult && hitResult.getType() != HitResult.Type.MISS) {
+                long time = player.getWorld().getTime();
+                if (time - lastBreak < 5) {
+                    return true;
+                }
+            }
+            return false;
+        });
+        ClientPlayerBlockBreakEvents.AFTER.register((world, player, pos, state) -> {
+            lastBreak = world.getTime();
+        });
     }
     @ApiStatus.Internal
     public static int removeDelay(ClientPlayerInteractionManager self, int original) {
-        return isOn() || !TCConfigs.A().isMiningDelay() ? original : 0;
-    }
-    public static void setDelay(ClientPlayerInteractionManager self, BlockState blockState, BlockPos blockPos, Direction direction, int sequence) {
-        if (MiningDelay.isOn() && TCConfigs.A().isMiningDelay()) {
-            var interactor = (ClientPlayerInteractionManager & AClientPlayerInteractionManager) self;
-            interactor.setBlockBreakingCooldown(5);
-        }
-    }
-    public static boolean isOn() {
-        return on;
+        return !TCConfigs.A().isMiningDelay() ? original : 0;
     }
     private static void onStartTick(ClientWorld world0) {
         if (!TCConfigs.A().isMiningDelay() || !TCKeyBindings.MINING_DELAY.wasPressed()) {
@@ -41,17 +48,7 @@ public class MiningDelay {
         while (TCKeyBindings.MINING_DELAY.wasPressed()) {
             //防止长按（？）
         }
-        var world = (ClientWorld & AClientWorld) world0;
         setOn(!isOn());
-        var interactor = (ClientPlayerInteractionManager & AClientPlayerInteractionManager) world.getClient().interactionManager;
-        //PlayerEntity player = MinecraftClient.getInstance().player;
-        if (interactor == null /*|| player == null*/) {
-            return;
-        }
-        if (isOn()) {
-            interactor.setBlockBreakingCooldown(5);
-        } else {
-            interactor.setBlockBreakingCooldown(0);
-        }
+        ((AClientPlayerInteractionManager) MCUtils.getInteractor()).setBlockBreakingCooldown(0);
     }
 }
--- src/main/java/phoupraw/mcmod/trifleclient/mixin/minecraft/MClientPlayerInteractionManager.java
+++ src/main/java/phoupraw/mcmod/trifleclient/mixin/minecraft/MClientPlayerInteractionManager.java
@@ -5,24 +5,17 @@ import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
-import net.minecraft.network.listener.ServerPlayPacketListener;
-import net.minecraft.network.packet.Packet;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import phoupraw.mcmod.trifleclient.events.AfterUseBlockCallback;
 import phoupraw.mcmod.trifleclient.misc.AutoSwitchTools;
 import phoupraw.mcmod.trifleclient.misc.MiningDelay;
@@ -36,10 +29,6 @@ abstract class MClientPlayerInteractionManager {
     @ModifyExpressionValue(method = {"attackBlock", "updateBlockBreakingProgress"}, at = @At(value = "CONSTANT", args = "intValue=5"))
     private int miningDelay(int original) {
         return MiningDelay.removeDelay((ClientPlayerInteractionManager) (Object) this, original);
-    }
-    @Inject(method = "method_41930", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerInteractionManager;breakBlock(Lnet/minecraft/util/math/BlockPos;)Z"))
-    private void miningDelay(BlockState blockState, BlockPos blockPos, Direction direction, int sequence, CallbackInfoReturnable<Packet<ServerPlayPacketListener>> cir) {
-        MiningDelay.setDelay((ClientPlayerInteractionManager) (Object) this, blockState, blockPos, direction, sequence);
     }
     @ModifyReturnValue(method = "interactBlockInternal",at = @At("RETURN"))
     private ActionResult afterUseBlock(ActionResult original, ClientPlayerEntity player, Hand hand, BlockHitResult hitResult) {
